a9eb4917c6e4440ca3d739393379f9d21f1b7fa6
Mark Mercer <Mark.Mercer@epsb.ca>
2024-03-08 12:14:56 -0700

    Add files via upload

---
+++ 06 Paddle Class/01 Drawing Paddles/PracticePong/Ball.java
@@ -0,0 +1,14 @@
+class Ball
+{
+  //Pure Java Class
+  //Primitive Vars Only
+  //
+  //Global Variables
+  //
+  //Overloaded Constructors
+  //
+  //void draw() {}
+  //
+  //VOIDs and Calculators
+  //
+} //End Ball.java
